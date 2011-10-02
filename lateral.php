dd607eebbf9cf84133d78d9f431cba4783054d68
Laura Vilib <laura_trea@hotmail.com>
2011-10-02 18:42:45 -0500

    style commit

---
+++ lateral.php
@@ -0,0 +1,12 @@
+<html>
+
+<div id="leftside">
+	<center>
+<a href="Inicio.php" name="entrar" class="menu_button blue medium">Inicio</a><br />
+<a href="BuscarUsuario.php" name="entrar" class="menu_button blue medium ">Busqueda de<br /> Usuario</a><br />
+<a href="Ver.php" name="entrar" class="menu_button blue medium">Busqueda de<br /> Documento</a><br /> 
+<a href="SubirDoc.php" name="entrar" class="menu_button blue medium">Subir<br /> Documento</a><br />
+	</center>
+</div>
+
+</html>
