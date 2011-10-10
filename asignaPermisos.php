88d47dc257784dd20bef777d80850ccdfbdf7439
Laura Vilib <laura_trea@hotmail.com>
2011-10-10 18:26:15 -0500

    ya no veremos facebook javan; Si amarras una tortuga a la espalda de otra, se convierte en un animal invencible

--- asignaPermisos.php
+++ asignaPermisos.php
@@ -10,26 +10,29 @@
 	
 	$id=$_GET['id'];
 
-	$query = "SELECT *
-			  FROM usuarios 
-			  WHERE N_Control = '$id'";	
+	$query = "SELECT N_Control
+                    ,usuarios.Nombre as 'Nombre'
+       				,Paterno,Materno
+	   				,Edad
+     				,Mail
+      				,tipo_usuario.Nombre as 'Tipo'
+      				,especialidad.Nombre as 'Especialidad'
+      				,departamento.Nombre as 'Dpto'
+			  FROM   usuarios
+                    ,tipo_usuario
+                    ,especialidad
+                    ,departamento
+			  WHERE usuarios.Id_tipo =tipo_usuario.Id_tipo 
+                    and usuarios.Id_Esp  =especialidad.Id_Esp
+                    and usuarios.Id_Dpto =departamento.Id_Dpto
+                    and N_Control='$id'";
 
 	$result = mysqli_query($conexion,$query) 
 							or die("no se pudo realizar la consulta");
 	 
 	$row = mysqli_fetch_assoc($result);
 	
-	$idid=$row['Id_tipo'];
-	
-	$qry2="SELECT * 
-		   FROM departamento
-		   WHERE Id_Dpto = $idid";
-		   
-	$resultado= mysqli_query($conexion,$qry2) 
-							or die("no se pudo realizar la consulta");
-							
-	$fila = mysqli_fetch_assoc($resultado);
-	
+		
 	?>
     
     <form action="" name="permisos" method="post">
@@ -46,13 +49,14 @@
         <input type="text" id="txtMail" value="<?php echo $row['Mail']?>"  disabled="disabled"/>
         <br />
         <label>Especialidad: </label>
-        <input type="text" id="txtEsp" value="<?php echo $row['Id_Esp']?>" disabled="disabled"/>
+        <input type="text" id="txtEsp" value="<?php echo $row['Especialidad']?>" disabled="disabled"/>
         <br />
         <label>Departamento :</label>
-        
-        <select name="listaDpto">
-        <input type="text" id="txtDpto" value="<?php echo $fila['Nombre']?>" />
-        </select>
+        <input type="text" id="txtDpto" value="<?php echo $row['Dpto']?>" />
+        <br>
+        <label>Tipo de Usuario: </label>
+        <input type="text" id="txtTipo" value="<?php echo $row['Tipo']?>" />
+   
         
     </form>
 
