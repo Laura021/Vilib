801e700ca71b6940ff2f93f8f275710f9efa9a79
Laura Vilib <laura_trea@hotmail.com>
2011-10-12 13:18:28 -0500

    pagina para controlar permisos

--- faltaPermisos.php
+++ faltaPermisos.php
@@ -1,3 +1,29 @@
 <?php
-    phpinfo();
+    session_start();
+	
+	include ("conexion.php");
 ?>
+<html>
+	<head>
+		<title>Falta de permisos</title>
+	    <link rel="stylesheet" href="style2.css" media="screen"/>
+	</head>
+	<body>
+		<div id="container">
+		<?php
+			
+	include ("cabecera.php");
+	
+	include ("lateral.php");
+		?>	
+		<div id="main">
+			
+		<div class="important">
+			"Usted no tiene los permisos para entrar a esta pagina :("
+		</div>
+			
+		</div><!-- fin del main -->
+			
+		</div><!--fin del container -->
+	</body>
+</html>
