02667f6a132c61fbe65b8694942ab294a1ffa90f
Laura Vilib <laura_trea@hotmail.com>
2011-10-12 13:04:05 -0500

    cambios en lateral

--- lateral.php
+++ lateral.php
@@ -1,11 +1,40 @@
 <html>
-
 <div id="leftside">
 	<center>
-<a href="Inicio.php" name="entrar" class="menu_button blue medium">Inicio</a><br /><br />
-<a href="BuscarUsuario.php" name="entrar" class="menu_button blue medium ">Busqueda de<br /> Usuario</a><br /><br />
-<a href="Ver.php" name="entrar" class="menu_button blue medium">Busqueda de<br /> Documento</a><br /> <br />
-<a href="SubirDoc.php" name="entrar" class="menu_button blue medium">Subir<br /> Documento</a><br /><br />
+<?php 
+	$usu=$_SESSION['tipo_usu'];
+	
+	if($usu == 1)
+	{?>
+		<a href="Inicio.php" name="entrar" class="menu_button blue medium">Inicio</a><br /><br />
+		<a href="BuscarUsuario.php" name="entrar" class="menu_button blue medium ">Busqueda de<br /> Usuario</a><br /><br />
+		<a href="Ver.php" name="entrar" class="menu_button blue medium">Busqueda de<br /> Documento</a><br /> <br />
+		<a href="SubirDoc.php" name="entrar" class="menu_button blue medium">Subir<br /> Documento</a><br /><br />
+		
+	<?php
+	}
+	if($usu == 2)
+	{?>
+		<a href="Inicio.php" name="entrar" class="menu_button blue medium">Inicio</a><br /><br />
+		<a href="BuscarUsuario.php" name="entrar" class="menu_button blue medium ">Busqueda de<br /> Usuario</a><br /><br />
+		<a href="Ver.php" name="entrar" class="menu_button blue medium">Busqueda de<br /> Documento</a><br /> <br />
+		<a href="SubirDoc.php" name="entrar" class="menu_button blue medium">Subir<br /> Documento</a><br /><br />
+
+	<?php		
+	}
+	if($usu == 3)
+	{?>
+		
+		<a href="Inicio.php" name="entrar" class="menu_button blue medium">Inicio</a><br /><br />
+		<a href="" name="entrar" class="menu_button blue medium ">Busqueda de<br /> Usuario</a><br /><br />
+		<a href="Ver.php" name="entrar" class="menu_button blue medium">Busqueda de<br /> Documento</a><br /> <br />
+		<a href="" name="entrar" class="menu_button blue medium">Subir<br /> Documento</a><br /><br />
+
+		
+	<?php }
+	
+?>
+
 	</center>
 </div>
 
