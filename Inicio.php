a14a4e8db4f481b45950a5262b39b4661c19b5f8
Javan Vilib <javienen@hotmail.com>
2011-11-15 01:41:07 -0600

    agregada informacion de Vilib en el incio de Vilib

--- Inicio.php
+++ Inicio.php
@@ -22,7 +22,26 @@ session_start();
 			<?php include ("lateral.php");?>
             
         	<div id="main">
-            
+            	<h2>¡Bienvenido a Vilib!</h2>
+            	<div class="descInicio">
+            	<p>Vilib es un Repositiorio/Biblioteca Virtual que alberga
+            	una gran cantidad de dodcumentos con datos interesantes
+            	sobre la carrera de Ing. en Sistemas Computacionales.</p>
+            	
+            	<p>Su principal objetivo es proporcionar informacion de
+            	calidad, categorizandola y gestionandola de una forma eficiente.</p>
+            	
+            	<p>Vilib cuenta con su propio visor online de archivos donde se
+            		soportan diferentes formatos, como videos, documentos de texto 
+            		plano, documentos PDF, imagenes y muchos mas.</p>
+            		
+            	<p>Vilib fue creado como un proyecto para el Instituto Tecnologico
+            		de saltillo, para saber mas de la Institucion, visita el
+            		siguiente enlace:</p>
+            		
+            		<p><a class="movingText center" href="http://www.its.mx">Instituto Tecnologico de Saltillo</a></p>
+            		
+            	</div>
             </div>
             
 		</div>
